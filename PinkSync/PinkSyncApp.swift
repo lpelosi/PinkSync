f5fe8fadc0b570a28be22a20cfed6f0a27b5b03f
Louis Pelosi <lrpelosi@gmail.com>
2026-04-23 15:44:14 -0400

    Initial Commit

---
+++ PinkSync/PinkSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinkSyncApp.swift
+//  PinkSync
+//
+//  Created by Louis Pelosi on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct PinkSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
